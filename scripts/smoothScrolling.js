878fedb19ffb54457cf1eecccb75ddf8b06e45d6
papertek <littlerock894@gmail.com>
2023-11-12 09:30:33 -0600

    add functionality to wedge and change designs

---
+++ scripts/smoothScrolling.js
@@ -0,0 +1,23 @@
+// Eaten from https://codepen.io/bramus/pen/ExaEqMJ
+
+window.addEventListener('DOMContentLoaded', () => {
+  const observer = new IntersectionObserver((entries) => {
+    // For each loop (array-like thing)
+    entries.forEach((sectionEntry) => {
+      const id = sectionEntry.target.getAttribute('id');
+
+      if (sectionEntry.intersectionRatio != 0) {
+        document.querySelector(`nav li a[href="#${id}"]`)
+            .parentElement.classList.add('active');
+      } else {
+        document.querySelector(`nav li a[href="#${id}"]`)
+            .parentElement.classList.remove('active');
+      }
+    });
+  });
+
+  // Track all sections that have an id applied
+  document.querySelectorAll('section[id]').forEach((section) => {
+    observer.observe(section);
+  });
+});
