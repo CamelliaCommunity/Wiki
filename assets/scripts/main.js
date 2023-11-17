fc77964d1b7d8640781c4455f1cbc9ed3f754159
papertek <littlerock894@gmail.com>
2023-11-17 00:34:07 -0600

    remove domcontentloaded (scrolling bugfix)

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -26,40 +26,38 @@ if (scrollToTop) {
 // Eaten from https://codepen.io/bramus/pen/ExaEqMJ
 // Highlight of Contents wedge when user is in a new section of the page
 // Combined pieces of code to display new sections and to highlight them
-window.addEventListener('DOMContentLoaded', function() {
-  const sections = document.querySelectorAll('h2[id]');
-  const wedgeItems = document.getElementById('wedgeItems');
-  // Scroll to item
-  const observer = new IntersectionObserver((entries) => {
-    entries.forEach((sectionEntry) => {
-      const id = sectionEntry.target.getAttribute('id');
-      const wedgeLink =
-          document.querySelector(`nav li a[href="#${id}"]`).parentElement;
-      if (sectionEntry.isIntersecting) {
-        wedgeLink.classList.add('active');
-      } else {
-        wedgeLink.classList.remove('active');
-      }
-    });
+const sections = document.querySelectorAll('h2[id]');
+const wedgeItems = document.getElementById('wedgeItems');
+// Scroll to item
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((sectionEntry) => {
+    const id = sectionEntry.target.getAttribute('id');
+    const wedgeLink =
+        document.querySelector(`nav li a[href="#${id}"]`).parentElement;
+    if (sectionEntry.isIntersecting) {
+      wedgeLink.classList.add('active');
+    } else {
+      wedgeLink.classList.remove('active');
+    }
   });
+});
 
-  // Display the sections in the content wedge
-  sections.forEach(section => {
-    const id = section.getAttribute('id');
-    const listItem = document.createElement('li');
-    const link = document.createElement('a');
-    link.href = `#${id}`;
-    link.textContent = section.textContent;
-    listItem.appendChild(link);
-    wedgeItems.querySelector('ul').appendChild(listItem);
+// Display the sections in the content wedge
+sections.forEach(section => {
+  const id = section.getAttribute('id');
+  const listItem = document.createElement('li');
+  const link = document.createElement('a');
+  link.href = `#${id}`;
+  link.textContent = section.textContent;
+  listItem.appendChild(link);
+  wedgeItems.querySelector('ul').appendChild(listItem);
 
-    const vertLine = document.createElement('div');
-    vertLine.classList.add('vertLine');
-    listItem.appendChild(vertLine);
+  const vertLine = document.createElement('div');
+  vertLine.classList.add('vertLine');
+  listItem.appendChild(vertLine);
 
-    // Track all sections that have an id applied
-    observer.observe(section);
-  });
+  // Track all sections that have an id applied
+  observer.observe(section);
 });
 
 // Add the sticky class to header when user scrolls
@@ -76,11 +74,7 @@ const scrollDetect = () => {
 window.addEventListener('scroll', scrollDetect);
 
 // Test code
-window.addEventListener('DOMContentLoaded', function() {
-  const sections = document.querySelectorAll('h2[id]');
-
-  sections.forEach(section => {
-    const id = section.getAttribute('id');
-    console.log(id);
-  });
+sections.forEach(section => {
+  const id = section.getAttribute('id');
+  console.log(id);
 });
