ca1e3ce11b9a90791c6866f148bd809815d81889
Flux <flustix@foxes4life.net>
2024-01-28 13:58:57 +0100

    Make login also work on https

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -269,7 +269,7 @@ loginBtn.addEventListener("click", (event) => {
 		const DISCORD_CLIENT_ID = "1169155506988929024";
 		const popupParams = "scrollbars=no,resizable=no,status=no,location=no,toolbar=no,menubar=no,width=833,height=654";
 		popupWindow = window.open(
-			`https://discord.com/oauth2/authorize?response_type=token&client_id=${DISCORD_CLIENT_ID}&scope=identify&redirect_uri=http://${window.location.host}/oauthComplete`,
+			`https://discord.com/oauth2/authorize?response_type=token&client_id=${DISCORD_CLIENT_ID}&scope=identify&redirect_uri=${window.location.protocol}//${window.location.host}/oauthComplete`,
 			"popup",
 			popupParams
 		);
